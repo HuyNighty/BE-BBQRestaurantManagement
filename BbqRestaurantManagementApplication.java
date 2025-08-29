7d34a20d8810d16f4ef3a62f294dec31ed1b8287
Huy <husyah0000@gmail.com>
2025-08-29 11:00:45 +0700

    main

---
+++ BbqRestaurantManagementApplication.java
@@ -0,0 +1,13 @@
+package com.huynguyen.bbqrestaurantmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbqRestaurantManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbqRestaurantManagementApplication.class, args);
+	}
+
+}
